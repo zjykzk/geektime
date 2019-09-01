a69859893bec0fda4a84f01bca405109e650fb20
zjykzk <zjykzk@gmail.com>
2019-09-01 12:55:49 +0800

    mv

--- m3u8.go
+++ m3u8.go
@@ -52,6 +52,26 @@ func listM3U8Paths(dir string) []string {
 	return m3u8Paths
 }
 
+func doesDownloadAllTS(m3u8Path string) (bool, error) {
+	ts, err := parseM3u8(m3u8Path)
+	if err != nil {
+		return false, err
+	}
+
+	dir := filepath.Dir(m3u8Path)
+	for _, t := range ts {
+		if ok, err := fileExists(filepath.Join(dir, t)); err == nil {
+			if !ok {
+				return false, nil
+			}
+		} else {
+			return false, err
+		}
+	}
+
+	return true, nil
+}
+
 // M3U8ToMP4Converter converts the videos with the m3u8 format to the mp4
 type M3U8ToMP4Converter struct {
 	outputDir, inputDir string
